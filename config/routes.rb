eca955816a8cb565ab6f1331be6cbb57f6d2eb87
William McLane <dukeoftinmen@gmail.com>
2017-10-04 03:32:56 +0000

    done Crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
-  get 'welcome/about'
+  resources :wikis
 
   devise_for :users
-  root to: "welcome#index"
+  
+  get 'about' => 'welcome#about'
+
+  root 'welcome#index'
 end
